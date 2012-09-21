087559ed8cc98d75a0f651d7159a381303725e56
schourode <jsr@malamute.dk>
2012-09-21 14:39:48 +0200

    Version 1.0 released via NuGet.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ISO 3166-1 Country codes")]
+[assembly: AssemblyDescription("Codes for the representation of names of countries.")]
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: ComVisible(false)]
