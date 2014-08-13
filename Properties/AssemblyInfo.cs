1c1f7aa0a41ea6bc1c52a26cf115fc8b80d11a50
schourode <jsr@malamute.dk>
2014-08-13 10:48:40 +0200

    Name changes to countries CV and PS, according to ISO.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ISO 3166-1 Country codes")]
 [assembly: AssemblyDescription("Codes for the representation of names of countries.")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 
 [assembly: ComVisible(false)]
