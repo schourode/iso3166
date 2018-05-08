ad52002496a9f806190ad7b98634424405334680
andersnm <andersnm@users.noreply.github.com>
2018-05-08 11:37:44 +0200

    Updated to new csproj format with netstandard1.0 target and Nuget settings

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,8 +1,4 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ISO 3166-1 Country codes")]
-[assembly: AssemblyDescription("Codes for the representation of names of countries.")]
-[assembly: AssemblyVersion("1.0.1.0")]
-
 [assembly: ComVisible(false)]
